a1348bacc5c7e29e47027a66d7966728af654596
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2025-10-30 15:37:34 -0400

    admin dashboard

--- src/components/NavigationMain.tsx
+++ src/components/NavigationMain.tsx
@@ -10,34 +10,43 @@ export const NavigationMain = () => {
   const { data: session } = useSession();
   const isAuthenticated = !!session?.user;
 
+  const userRole = session?.user?.role;
+
   const handleSignOut = () => {
     signOut({ callbackUrl: '/' });
   };
 
   return (
     <nav>
-      <div className="nav-link-container">
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            {isAuthenticated ? (
-              <Menu
-                buttonText='Account'
-              >
-                <MenuItem>
-                  <Link href="/account">Account</Link>
-                </MenuItem>
-                <MenuItem>
-                  <Button onClick={handleSignOut}>Sign Out</Button>
-                </MenuItem>
-              </Menu>
-            ) : (
-              <Link href="/sign-in">Sign In</Link>
-            )}
-          </li>
-        </ul>
+      <div className="nav-container">
+        <div className="nav-link-container">
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              {isAuthenticated ? (
+                <Menu
+                  buttonText='Account'
+                >
+                  <MenuItem>
+                    <Link href="/account">Settings</Link>
+                  </MenuItem>
+                  {(userRole === 'ADMIN' || userRole === 'SUPER') && (
+                    <MenuItem>
+                      <Link href="/admin">Admin Dashboard</Link>
+                    </MenuItem>
+                  )}
+                  <MenuItem>
+                    <Button onClick={handleSignOut}>Sign Out</Button>
+                  </MenuItem>
+                </Menu>
+              ) : (
+                <Link href="/sign-in">Sign In</Link>
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );

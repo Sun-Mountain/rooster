722bd30d3da0636dfc1ae19c9a8133bd9cf4ccb5
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2025-12-09 14:14:15 -0500

    add additional to auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,3 +1,19 @@
-import { createAuthClient } from 'better-auth/react'
+import { createAuthClient } from 'better-auth/react';
+import { inferAdditionalFields } from 'better-auth/client/plugins';
 
-export const { signIn, signUp, signOut, useSession } = createAuthClient()
+export const { signIn, signUp, signOut, useSession } = createAuthClient({
+  plugins: [
+    inferAdditionalFields({
+      user: {
+        firstName: {
+          type: 'string',
+          required: true
+        },
+        lastName: {
+          type: 'string',
+          required: true
+        },
+      }
+    })
+  ]
+})

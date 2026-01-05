6cbd236d7d8a389e9010cf36499bf09c3942014f
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2026-01-05 12:40:41 -0500

    update migrations

--- prisma/migrations/20251213223516_change_second_email_to_secondary_email/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `secondEmail` on the `contact_address` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "contact_address" DROP COLUMN "secondEmail",
-ADD COLUMN     "secondaryEmail" TEXT;

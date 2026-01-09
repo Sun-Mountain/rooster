3ddf623d985f887a696f0e3cf069052cd3940601
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2026-01-08 20:26:07 -0500

    added seed

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter});
+
+
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      firstName: 'Alice',
+      lastName: 'Wonderland',
+      name: 'Alice Wonderland',
+      role: 'ADMIN',
+      emailVerified: true,
+    },
+  });
+  console.log({ alice });
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })

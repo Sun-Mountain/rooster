5b5ed9d62d673813faf56c37e4eab1d9affaeb97
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2026-02-16 11:57:06 -0500

    reset schema

--- prisma/migrations/20260216165634_init/migration.sql
+++ prisma/migrations/20260216165634_init/migration.sql
@@ -6,6 +6,9 @@ CREATE TABLE "term" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "live" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -34,10 +37,10 @@ CREATE TABLE "contact_address" (
 -- CreateTable
 CREATE TABLE "emergency_contact" (
     "id" TEXT NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
     "relationship" TEXT,
-    "phone" TEXT NOT NULL,
+    "phone" TEXT,
     "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,

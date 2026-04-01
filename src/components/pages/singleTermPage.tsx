8cca8c17d44e1dd821085ca794fe088e8045c375
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2026-04-01 17:43:10 -0400

    single term page

--- src/components/pages/singleTermPage.tsx
+++ src/components/pages/singleTermPage.tsx
@@ -3,11 +3,12 @@
 import { useEffect, useState } from "react";
 import { TermProps } from "@/lib/props";
 import { usePathname } from "next/navigation";
+import { Edit, Delete } from "@mui/icons-material";
 import { Breadcrumbs } from "@/components/_ui/Breadcrumbs";
 import { fetchSingleTermById, updateTermStatusById } from "@/lib/api/term";
 import { dateFormat } from "@/helpers/dateFormatting";
 import { getStatusIcon } from "@/components/_ui/TermStatusIcon";
-import { Button } from "../_ui/Button";
+import { Button } from "@/components/_ui/Button";
 
 export default function SingleTermPageContent() {
   const pathname = usePathname();
@@ -45,23 +46,42 @@ export default function SingleTermPageContent() {
     <div className="admin-page-container">
       <Breadcrumbs currentPageTitle={`Session: ${pageTitle}`} />
       <h1>{pageTitle}</h1>
-      <div className="content-container">
-        <div className="term-dates-container">
-          {termData ? dateFormat(termData.startDate) : ""} - {termData ? dateFormat(termData.endDate) : ""}
-        </div>
-        <div className="term-status-container">
-          {termData?.status === "ENDED" ? (
-            <>
-              {termData ? getStatusIcon(termData.status, termData.endDate) : null}
-              {termData ? termData.status.slice(0,1) + termData.status.slice(1).toLowerCase() : ""}
-            </>
-          ) : (
-            <Button className="w-icon">
-              {termData ? getStatusIcon(termData.status) : null}
-              {termData ? termData.status.slice(0,1) + termData.status.slice(1).toLowerCase() : ""}
+      <div className="content-container two-column">
+        <div>
+          <div className="action-btns-container">
+            <Button className="w-icon small">
+              <Edit /> Edit
             </Button>
-          )}
+            <Button className="w-icon danger small">
+              <Delete /> Delete
+            </Button>
+          </div>
         </div>
+        <div>
+          <div className="term-dates-container">
+            {termData ? dateFormat(termData.startDate) : ""} - {termData ? dateFormat(termData.endDate) : ""}
+          </div>
+          <div className="term-status-container">
+            {termData?.status === "ENDED" ? (
+              <>
+                {termData ? getStatusIcon(termData.status, termData.endDate) : null}
+                {termData ? termData.status.slice(0,1) + termData.status.slice(1).toLowerCase() : ""}
+              </>
+            ) : (
+              <Button className="w-icon">
+                {termData ? getStatusIcon(termData.status) : null}
+                {termData ? termData.status.slice(0,1) + termData.status.slice(1).toLowerCase() : ""}
+              </Button>
+            )}
+          </div>
+          <div className="term-description-container">
+            {termData?.description ? termData.description : (
+              <em>No description provided.</em>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="content-container admin-list">
       </div>
     </div>
   )

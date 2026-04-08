155fb53e2dac5c7b3e7d130072a03740f03930de
PatKelsh <pkelsh@gmail.com>
2026-04-08 16:00:50 -0400

    updated terraform to simplify enviroment

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "aws_ecr_repo_url" {
 output "aws_ecr_repo_arn" {
   value = aws_ecr_repository.repository.arn
 }
+
+output "aws_ec2_instance_id" {
+  value = aws_instance.app_server.id
+}
+
+output "aws_ec2_instance_public_ip" {
+  value = aws_instance.app_server.public_ip
+}

706692fb42b7894698fabfacc500b9fa8c2fb8f5
Pat Kelsh <pkelsh@fearless.tech>
2026-04-29 11:07:00 -0400

    Add in user update function and function to find all active users

--- src/lib/prisma/user.ts
+++ src/lib/prisma/user.ts
@@ -1,27 +1,45 @@
 import db from "@/lib/prisma";
 import { User, Prisma, Role } from "@client";
 
+// Get a list of all users
 export const getAllUsers = async (): Promise<User[]> => {
   return await db.user.findMany();
 };
 
+// Get a list of all active users
+export const getAllActiveUsers = async (): Promise<User[]> => {
+	return await db.user.findMany({
+		where { active: true }
+	});
+};
+
+// Get user by a specified user id
 export const getUserById = async (id: string): Promise<User | null> => {
   return await db.user.findUnique({
     where: { id },
   });
 };
 
+// Delete a user from the database
 export const deleteUserById = async (id: string): Promise<User> => {
   return await db.user.delete({
     where: { id },
   });
 };
 
+// Udate info for a single user based on a user id. The user id will remain the same but all other fields may be changed
 export const updateUserInfo = async (userId: string, data: Prisma.UserUpdateInput): Promise<User> => {
   return await db.contactAddress.update({
     where: {
       id: userId,
     },
+    data,
+  });
+};
+
+// Create a new user in the database
+export const createUser = async (data: Prisma.UserCreateInput): Promise<User> => {
+  return await db.user.create({
     data,
   });
 };

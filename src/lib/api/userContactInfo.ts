c00e060af9880a9c1d8ab32459f16f445dc590d8
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2026-08-13 11:58:12 -0400

    boop

--- src/lib/api/userContactInfo.ts
+++ src/lib/api/userContactInfo.ts
@@ -1,8 +1,7 @@
 export const getOrCreateContactInfo = async (userId: string) => {
   try {
     const contactInfo = await fetch(`/api/user/contact?userId=${userId}`);
-    const contactInfoJSON = await contactInfo.json();
-    if (contactInfoJSON === null) {
+    if (contactInfo === null) {
       const newContactInfo = await fetch(`/api/user/contact`, {
         method: "POST",
         headers: {
@@ -19,6 +18,7 @@ export const getOrCreateContactInfo = async (userId: string) => {
       });
       return newContactInfo.json();
     } else {
+      const contactInfoJSON = await contactInfo.json();
       return contactInfoJSON;
     }
   } catch (error) {
